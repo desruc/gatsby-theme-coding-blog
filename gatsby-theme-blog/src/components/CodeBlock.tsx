f10a8d0cb11ca73735968ebf5fe616b7500829cb
James Cameron <jdonaldcameron@gmail.com>
2020-05-13 08:08:09 +1000

    feat: add language tag and some styling to code block

--- gatsby-theme-blog/src/components/CodeBlock.tsx
+++ gatsby-theme-blog/src/components/CodeBlock.tsx
@@ -7,21 +7,26 @@ type CodeBlockProps = {
   [key: string]: any;
 };
 
-const CodeBlock = ({ codeString, className: codeClassName, ...props }: CodeBlockProps) => {
+const CodeBlock = ({ codeString, noLineNumbers = false, className: codeClassName, ...props }: CodeBlockProps) => {
   const matches = codeClassName.match(/language-(?<lang>.*)/);
   const language = matches && matches.groups && matches.groups.lang ? matches.groups.lang : ``;
+  const hasLineNumbers = !noLineNumbers && language !== `noLineNumbers`;
+
   return (
     <Highlight {...defaultProps} code={codeString} language={language} theme={nightOwl}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={style}>
-          {tokens.map((line, i) => (
-            <div {...getLineProps({ line, key: i })}>
-              {line.map((token, key) => (
-                <span {...getTokenProps({ token, key })} />
-              ))}
-            </div>
-          ))}
-        </pre>
+        <div className="gatsby-highlight" data-language={language}>
+          <pre className={className} style={style} data-linenumber={hasLineNumbers}>
+            {tokens.map((line, i) => (
+              <div {...getLineProps({ line, key: i })}>
+                {hasLineNumbers && <span className="line-number-style">{i + 1}</span>}
+                {line.map((token, key) => (
+                  <span {...getTokenProps({ token, key })} />
+                ))}
+              </div>
+            ))}
+          </pre>
+        </div>
       )}
     </Highlight>
   );
--- gatsby-theme-blog/src/components/Layout.tsx
+++ gatsby-theme-blog/src/components/Layout.tsx
@@ -1,5 +1,7 @@
+/** @jsx jsx */
 import React from "react";
-import { Box, Container } from "theme-ui";
+import { jsx, Box, Container } from "theme-ui";
+import CodeStyles from "../styles/code";
 
 import Seo from "./Seo";
 import Header from "./Header";
@@ -8,14 +10,14 @@ import Footer from "./Footer";
 type LayoutProps = { children: React.ReactNode };
 
 const Layout = ({ children }: LayoutProps) => (
-  <>
+  <React.Fragment>
     <Seo />
     <Container>
       <Header />
-      <Box>{children}</Box>
+      <Box sx={{ ...CodeStyles }}>{children}</Box>
       <Footer />
     </Container>
-  </>
+  </React.Fragment>
 );
 
 export default Layout;
