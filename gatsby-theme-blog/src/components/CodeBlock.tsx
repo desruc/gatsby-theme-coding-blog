887d1995ba8c4e6c330a42eeaaba405ce5949e10
James Cameron <jdonaldcameron@gmail.com>
2020-07-07 07:30:06 +1000

    feat: finalize code block themes

--- gatsby-theme-blog/src/components/CodeBlock.tsx
+++ gatsby-theme-blog/src/components/CodeBlock.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import Highlight, { defaultProps } from "prism-react-renderer";
+import { useColorMode } from "theme-ui";
+
+import Highlight, { defaultProps, PrismTheme } from "prism-react-renderer";
+import gitHubTheme from "prism-react-renderer/themes/github";
+import duotoneDark from "prism-react-renderer/themes/oceanicNext";
 import nightOwl from "prism-react-renderer/themes/nightOwl";
 
 type CodeBlockProps = {
@@ -7,13 +11,21 @@ type CodeBlockProps = {
   [key: string]: any;
 };
 
-const CodeBlock = ({ codeString, noLineNumbers = false, className: codeClassName, ...props }: CodeBlockProps) => {
+const CodeBlock = ({ codeString, noLineNumbers = false, className: codeClassName }: CodeBlockProps) => {
+  const [colorMode] = useColorMode();
+
+  const themes: { [key: string]: PrismTheme } = {
+    light: gitHubTheme,
+    green: duotoneDark,
+    dark: nightOwl,
+  };
+
   const matches = codeClassName.match(/language-(?<lang>.*)/);
   const language = matches && matches.groups && matches.groups.lang ? matches.groups.lang : ``;
   const hasLineNumbers = !noLineNumbers && language !== `noLineNumbers`;
 
   return (
-    <Highlight {...defaultProps} code={codeString} language={language} theme={nightOwl}>
+    <Highlight {...defaultProps} code={codeString} language={language} theme={themes[colorMode]}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <div className="gatsby-highlight" data-language={language}>
           <pre className={className} style={style} data-linenumber={hasLineNumbers}>
--- gatsby-theme-blog/src/components/Layout.tsx
+++ gatsby-theme-blog/src/components/Layout.tsx
@@ -1,6 +1,6 @@
 /** @jsx jsx */
 import React from "react";
-import { jsx, Box, Container } from "theme-ui";
+import { jsx, Box, Container, useColorMode } from "theme-ui";
 import { Global } from "@emotion/core";
 import CodeStyles from "../styles/code";
 
@@ -10,22 +10,25 @@ import Footer from "./Footer";
 
 type LayoutProps = { children: React.ReactNode };
 
-const Layout = ({ children }: LayoutProps) => (
-  <React.Fragment>
-    <Global
-      styles={() => ({
-        body: {
-          transition: `all 0.3s ease-in-out`,
-        },
-      })}
-    />
-    <Seo />
-    <Container>
-      <Header />
-      <Box sx={{ ...CodeStyles }}>{children}</Box>
-      <Footer />
-    </Container>
-  </React.Fragment>
-);
+const Layout = ({ children }: LayoutProps) => {
+  const [colorMode] = useColorMode();
+  return (
+    <React.Fragment>
+      <Global
+        styles={() => ({
+          body: {
+            transition: `all 0.3s ease-in-out`,
+          },
+        })}
+      />
+      <Seo />
+      <Container>
+        <Header />
+        <Box sx={{ ...CodeStyles(colorMode) }}>{children}</Box>
+        <Footer />
+      </Container>
+    </React.Fragment>
+  );
+};
 
 export default Layout;
